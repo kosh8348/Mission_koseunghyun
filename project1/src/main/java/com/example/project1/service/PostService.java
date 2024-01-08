ac27aa5e326aceadcb571a9dcc8370cf7b641f89
koseunghyun <kosh8348@gmail.com>
2024-01-08 16:52:41 +0900

    PostService 인터페이스 구현

---
+++ project1/src/main/java/com/example/project1/service/PostService.java
@@ -0,0 +1,13 @@
+package com.example.project1.service;
+
+import com.example.project1.entity.Post;
+
+import java.util.List;
+
+public interface PostService {
+    List<Post> getAllPosts();
+    Post getPostById(Long id);
+    Post createPost(Post post);
+    Post updatePost(Long id, Post updatedPost, String authorPassword);
+    void deletePost(Long id, String authorPassword);
+}
