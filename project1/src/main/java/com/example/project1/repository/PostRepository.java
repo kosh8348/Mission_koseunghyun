039d71bc4477b3845da624c2441b79e0605fdff7
koseunghyun <kosh8348@gmail.com>
2024-01-09 18:04:54 +0900

    annotation 추가

--- project1/src/main/java/com/example/project1/repository/PostRepository.java
+++ project1/src/main/java/com/example/project1/repository/PostRepository.java
@@ -1,8 +1,9 @@
 package com.example.project1.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.project1.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
-    // 추가적인 메서드가 필요하면 여기에 작성할 수 있습니다.
 }
