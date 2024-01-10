5d54ac88103a0bfeb38231f411fb396f13515217
koseunghyun <kosh8348@gmail.com>
2024-01-10 15:37:52 +0900

    modify

--- project1/src/main/java/com/example/project1/controller/PostController.java
+++ project1/src/main/java/com/example/project1/controller/PostController.java
@@ -1,41 +1,31 @@
 package com.example.project1.controller;
 
+import com.example.project1.entity.Comment;
 import com.example.project1.entity.Post;
+import com.example.project1.service.CommentService;
 import com.example.project1.service.PostService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/boards/{boardId}/article")
+@RequestMapping("/posts")
 public class PostController {
-
     private final PostService postService;
+    private final CommentService commentService;
 
-    public PostController(PostService postService) {
+    public PostController(PostService postService, CommentService commentService) {
         this.postService = postService;
+        this.commentService = commentService;
     }
 
-    @GetMapping("/{articleId}")
-    public ResponseEntity<Post> getPostById(@PathVariable Long articleId) {
-        Post post = postService.getPostById(articleId);
-        return ResponseEntity.ok().body(post);
+    @GetMapping("/{postId}")
+    public Post getPost(@PathVariable Long postId) {
+        return postService.getPostById(postId);
     }
 
-    @PostMapping("/")
-    public ResponseEntity<Post> createPost(@RequestBody Post post, @PathVariable Long boardId, @RequestParam String authorPassword) {
-        Post createdPost = postService.createPost(post, boardId, authorPassword);
-        return ResponseEntity.ok().body(createdPost);
-    }
-
-    @PutMapping("/{articleId}")
-    public ResponseEntity<Post> updatePost(@PathVariable Long articleId, @RequestBody Post updatedPost, @RequestParam String authorPassword) {
-        Post post = postService.updatePost(articleId, updatedPost, authorPassword);
-        return ResponseEntity.ok().body(post);
-    }
-
-    @DeleteMapping("/{articleId}")
-    public ResponseEntity<?> deletePost(@PathVariable Long articleId, @RequestParam String authorPassword) {
-        postService.deletePost(articleId, authorPassword);
-        return ResponseEntity.ok().build();
+    @PostMapping("/{postId}/comments")
+    public Comment addComment(@PathVariable Long postId, @RequestBody Comment comment) {
+        Post post = postService.getPostById(postId);
+        comment.setPost(post);
+        return commentService.addComment(comment);
     }
 }
