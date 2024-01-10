f456d9b9b631e432e2120ee9490514e2e7045722
koseunghyun <kosh8348@gmail.com>
2024-01-10 15:34:24 +0900

    메서드 변경

--- project1/src/main/java/com/example/project1/controller/BoardController.java
+++ project1/src/main/java/com/example/project1/controller/BoardController.java
@@ -1,8 +1,8 @@
 package com.example.project1.controller;
 
 import com.example.project1.entity.Board;
+import com.example.project1.entity.Post;
 import com.example.project1.service.BoardService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,24 +13,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/boards")
 public class BoardController {
-
     private final BoardService boardService;
 
     public BoardController(BoardService boardService) {
         this.boardService = boardService;
     }
 
-    @GetMapping("/list")  // 게시판 리스트
+    @GetMapping
     public List<Board> getAllBoards() {
         return boardService.getAllBoards();
     }
 
-
-    @GetMapping("/{boardId}")   // 앤드포인트 (게시판 보기)
-    public ResponseEntity<Board> getBoard(@PathVariable Long boardId) {
+    @GetMapping("/{boardId}/posts")
+    public List<Post> getPostsByBoard(@PathVariable Board boardId) {
         Board board = boardService.getBoardById(boardId);
-        return ResponseEntity.ok().body(board);
+        BoardController postService = null;
+        return postService.getPostsByBoard(board);
     }
-
-    // Other CRUD endpoints for boards (create, update, delete) can be added here
 }
