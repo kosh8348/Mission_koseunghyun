27cbfc4aca81c755acc33e4ec8a80c2b9e93c74a
koseunghyun <kosh8348@gmail.com>
2024-01-10 15:35:04 +0900

    Post에 대한 댓글 목록을 조회하는 findByPost 메서드를 추가

--- project1/src/main/java/com/example/project1/repository/CommentRepository.java
+++ project1/src/main/java/com/example/project1/repository/CommentRepository.java
@@ -1,9 +1,13 @@
 package com.example.project1.repository;
 
 import com.example.project1.entity.Comment;
+import com.example.project1.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
 }
