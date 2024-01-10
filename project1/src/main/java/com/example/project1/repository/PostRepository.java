f0b5e195eb8c1e51c67ba22a5f96fd1f6332153d
koseunghyun <kosh8348@gmail.com>
2024-01-10 15:38:23 +0900

    메서드 추가

--- project1/src/main/java/com/example/project1/repository/PostRepository.java
+++ project1/src/main/java/com/example/project1/repository/PostRepository.java
@@ -1,9 +1,13 @@
 package com.example.project1.repository;
 
+import com.example.project1.entity.Board;
 import com.example.project1.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByBoardOrderByCreatedDateDesc(Board board);
 }
