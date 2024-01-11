59809a60689c3ed9fe2e9cb1f101e9f5e9129f96
koseunghyun <kosh8348@gmail.com>
2024-01-11 15:31:06 +0900

    board 엔티티 간략화

--- project1/src/main/java/com/example/project1/entity/Board.java
+++ project1/src/main/java/com/example/project1/entity/Board.java
@@ -1,27 +1,11 @@
 package com.example.project1.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 @Entity
 public class Board {
-
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotNull
-    @Size(max = 255)
     private String name;
-
-    @NotNull
-    @Size(max = 1000)
-    private String description;
-
-    // constructors, getters, setters
 }
