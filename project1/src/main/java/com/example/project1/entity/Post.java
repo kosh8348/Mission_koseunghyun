3b0fc9a0681b479382002ba8fe5be240c8cc4076
koseunghyun <kosh8348@gmail.com>
2024-01-11 15:32:07 +0900

    Post 엔티티 메서드 수정

--- project1/src/main/java/com/example/project1/entity/Post.java
+++ project1/src/main/java/com/example/project1/entity/Post.java
@@ -4,21 +4,53 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import org.springframework.data.repository.CrudRepository;
 
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @NotNull
     private String title;
-
-    @NotNull
     private String content;
+    private String password; // 비밀번호 필드 추가
+    private Date createdDate;
 
-    private LocalDateTime createdAt;
+    @ManyToOne
+    private Board board; // 다대일 관계 설정
+
+    // 생성자, 게시판 정보를 받아와 설정
+    public Post(Board board, String title, String content, String password) {
+        this.board = board;
+        this.title = title;
+        this.content = content;
+        this.password = password;
+        this.createdDate = new Date();
+    }
+
+    public Post() {
+
+    }
+
+    public Post(Long boardId, String title, String content, String password) {
+    }
+
+    public String getPassword() {
+        return null;
+    }
+
+    public void setTitle(String title) {
+    }
+
+    public void setContent(String content) {
+
+    }
+
+    public interface PostRepository extends CrudRepository<Post, Long> {
+        List<Post> findByBoardIdOrderByCreatedDateDesc(Long boardId);
+    }
 }
